212d62a16cd8b84f733cb06b48eabbb4a5ca350b
Hirohito Kato <hkato193@gmail.com>
2023-03-25 23:02:29 +0900

    add template html

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello world from render.com!</p>"
+
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html", tag="iosdc")
